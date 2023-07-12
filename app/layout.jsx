9130fbdf801cb03af57dfac82a5b8b96e9882f44
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-12 17:32:44 +0330

    Setup layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,18 @@
-function Layout() {
-  return <html></html>;
+import "@/styles/global.css";
+
+export const metadata = {
+  title: "Landing page",
+  description: "Develop a landing page as  a task challenge",
+};
+
+function layout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <main className="px-[10%]">{children}</main>
+      </body>
+    </html>
+  );
 }
 
-export default Layout;
+export default layout;
