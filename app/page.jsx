7a428b87f689156d6bb48cec1a91a7b48d975c3d
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-13 18:37:02 +0330

    Add Introduction compoennt

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Main from "@/components/landingPage/Main";
+import Introduction from "@/components/introduction/Introduction";
 
 function Home() {
   return (
     <>
       <Main />
+      <Introduction />
     </>
   );
 }
