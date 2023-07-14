68e17188e0cffdd2925705449702be9e7ea95256
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-14 16:45:37 +0330

    Add about component

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Main from "@/components/landingPage/Main";
 import Introduction from "@/components/introduction/Introduction";
 import Testimonials from "@/components/testimonials/Testimonials";
 import Features from "@/components/features/Features";
+import About from "@/components/about/About";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <Introduction />
       <Testimonials />
       <Features />
+      <About />
     </>
   );
 }
---
+++ components/about/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+  return (
+    <main className="mt-[30vh] px-[10%] grid grid-cols-2 gap-[10vw]"></main>
+  );
+}
+
+export default About;
