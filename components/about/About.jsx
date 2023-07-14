c7e08a841d9ab9431e34a827e2bbd87d23896161
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-14 17:06:32 +0330

    set up about component

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -1,6 +1,23 @@
+import Image from "next/image";
+
 function About() {
   return (
-    <main className="mt-[30vh] px-[10%] grid grid-cols-2 gap-[10vw]"></main>
+    <main className="mt-[30vh] px-[10%] grid grid-cols-2 justify-center content-center gap-[5vw]">
+      <section className="relative min-h-[400px] w-full">
+        <Image src="/about.jpg" fill={true} />
+      </section>
+      <section className="flex flex-col justify-center items-start gap-8 text-teal-900">
+        <h2 className="text-cyan-500 font-semibold text-xs">About</h2>
+        <h3 className="font-semibold text-3xl ">
+          Some more information about your business
+        </h3>
+        <p className="font-normal text-sm">
+          Share a little about yourself as a business owner, or maybe describe
+          what makes your service unique. Give visitors one more reason to care
+          about your offer and want to work with you.
+        </p>
+      </section>
+    </main>
   );
 }
 
