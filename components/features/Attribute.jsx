98f40e187c1185144c33f4ad35e787f396d3c34a
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-14 16:39:09 +0330

    Set up the features component

--- components/features/Attribute.jsx
+++ components/features/Attribute.jsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 function Attribute({ img, title, description }) {
-  return <section></section>;
+  return (
+    <section className="flex flex-col justify-start items-start gap-5 text-teal-900">
+      <Image src={img} alt={title} width={300} height={300} />
+      <h2 className="font-semibold text-lg uppercase">{title}</h2>
+      <p className="font-normal text-sm w-3/4">{description}</p>
+    </section>
+  );
 }
 
 export default Attribute;
