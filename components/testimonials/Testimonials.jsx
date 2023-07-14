f81ef698b8e337048a65c14e1482d9f049d9c73e
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-14 13:19:22 +0330

    Add testemonials component

---
+++ components/testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimonials() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-12">
+      <h2 className="text-xs font-semibold text-cyan-500">
+        DON'T JUST TAKE OUR WORD FOR IT
+      </h2>
+    </div>
+  );
+}
+
+export default Testimonials;
