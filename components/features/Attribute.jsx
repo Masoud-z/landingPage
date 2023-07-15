cae35ee689eb4497a3ffe8c7e1407d4b439e1e6b
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-15 12:06:29 +0330

    Make Features component responssive

--- components/features/Attribute.jsx
+++ components/features/Attribute.jsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 function Attribute({ img, title, description }) {
   return (
     <section className="flex flex-col justify-start items-start gap-5 text-teal-900">
-      <Image src={img} alt={title} width={300} height={300} />
+      <Image
+        src={img}
+        alt={title}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+      />
       <h2 className="font-semibold text-lg uppercase">{title}</h2>
       <p className="font-normal text-sm w-3/4">{description}</p>
     </section>
--- components/features/Features.jsx
+++ components/features/Features.jsx
@@ -24,7 +24,7 @@ const features = [
 
 function Features() {
   return (
-    <main className="mt-[30vh] px-[10%] grid grid-cols-3 gap-3">
+    <main className="mt-20 md:mt-[30vh] px-[4%] md:px-[10%] grid grid-cols-1 md:grid-cols-3 gap-9 md:gap-3">
       {features.map((feature) => (
         <Attribute
           img={feature.img}
