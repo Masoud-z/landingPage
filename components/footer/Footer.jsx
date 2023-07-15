30db96c198da152df96421ca46dfae9ab3bf775a
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-15 12:16:57 +0330

    Make Footer component responssive

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="mt-[30vh] px-[10%] py-[10vh] bg-slate-800 flex justify-between items-center text-slate-500">
+    <footer className="mt-20 md:mt-[30vh] px-[4%] md:px-[10%] py-6 md:py-[10vh] bg-slate-800 flex flex-col md:flex-row justify-between items-center gap-3 text-slate-500">
       <div className="logo">Masoud</div>
       <p className="font-normal text-xs ">© Masoud. All rights reserved.</p>
     </footer>
